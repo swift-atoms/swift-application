06a7cb2c10e3ff5ffcb38880c174bd1fe24aba65
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:41:25 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-application-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: "Application Primitives",
             targets: ["Application Primitives"]
-        ),
+        )
     ],
     targets: [
         .target(
@@ -24,7 +24,7 @@ let package = Package(
         .testTarget(
             name: "Application Primitives Tests",
             dependencies: [
-                "Application Primitives",
+                "Application Primitives"
             ]
         ),
     ],
